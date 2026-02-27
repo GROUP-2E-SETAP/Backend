101abd6f0cbd40f34e14f3b2207d1a4869fadeeb
Bhavin367 <bhavinajay2006@gmail.com>
2026-02-27 11:27:01 +0000

    Removed all routes except auth, will be added later

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,37 +8,5 @@ import authRoutes from './v1/authRoutes.js';
 router.use('/auth', authRoutes);
 
 // Placeholder for other routes - these need to be converted to ES6
-// TODO: Convert all route files to ES6 modules
-router.get('/categories', (req, res) => {
-  res.json({ message: 'Categories endpoint - under construction' });
-});
-
-router.get('/transactions', (req, res) => {
-  res.json({ message: 'Transactions endpoint - under construction' });
-});
-
-router.get('/budgets', (req, res) => {
-  res.json({ message: 'Budgets endpoint - under construction' });
-});
-
-router.get('/dashboard', (req, res) => {
-  res.json({ message: 'Dashboard endpoint - under construction' });
-});
-
-router.get('/gamification', (req, res) => {
-  res.json({ message: 'Gamification endpoint - under construction' });
-});
-
-router.get('/notifications', (req, res) => {
-  res.json({ message: 'Notifications endpoint - under construction' });
-});
-
-router.get('/predictions', (req, res) => {
-  res.json({ message: 'Predictions endpoint - under construction' });
-});
-
-router.get('/price-comparison', (req, res) => {
-  res.json({ message: 'Price comparison endpoint - under construction' });
-});
 
 export default router;
